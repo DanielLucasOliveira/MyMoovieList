6eefc1b67c29785350cf9f738af67d167725548f
Italo Vilas Boas <vpn-990882@adm.bancoamazonia.sa>
2023-11-20 21:32:01 -0300

    commit teste

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { TesteComponent } from './teste/teste.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    TesteComponent
   ],
   imports: [
     CommonModule
