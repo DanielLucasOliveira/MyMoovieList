d5354b6df3b0115ed8c28a836ad57487a839eef9
Daniel Lucas <daniel.lucas1507@gmail.com>
2023-11-20 22:26:17 -0300

    ajuste de rotas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { TesteComponent } from './teste/teste.component';
+import { RoutingModule } from './routing.module';
 
 
 
@@ -10,10 +10,10 @@ import { TesteComponent } from './teste/teste.component';
   declarations: [
     HomeComponent,
     LoginComponent,
-    TesteComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RoutingModule
   ]
 })
 export class PagesModule { }
---
+++ src/app/pages/routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//Components
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'Login',
+        component: LoginComponent
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class RoutingModule { }
