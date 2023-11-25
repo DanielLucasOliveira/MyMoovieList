4a59d7c272e1a81587737dcbf943cca8fb4df161
Daniel Lucas <daniel.lucas1507@gmail.com>
2023-11-25 11:36:23 -0300

    modal

--- src/app/pages/components/create_list/create_list.component.ts
+++ src/app/pages/components/create_list/create_list.component.ts
@@ -12,34 +12,39 @@ export class CreateListComponent {
   isPrivate: boolean = false;
   idUsuario!: number;
 
-  @Input() listName!:string;
-  @Input() isPublic!:string;
-  @Input() usuarioId!:string;
+  @Input() listName!: string;
+  @Input() isPublic!: string;
+  @Input() usuarioId!: string;
   @Input() resultadoCriarLista!: string;
-  @Output() resultadoCriarListaChange: EventEmitter<string> = new EventEmitter<string>();;
-  
-  constructor(private listaService: ListaService, private route : ActivatedRoute, private router: Router) {}
+  @Output() resultadoCriarListaChange: EventEmitter<string> = new EventEmitter<string>();
+  @Output() mostarModal: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    ngOnInit(): void {
-      this.getID();
-    }
-  
-    getID(){
-      this.idUsuario = Number(this.route.snapshot.paramMap.get('idSession'));
-    }
+  constructor(private listaService: ListaService, private route: ActivatedRoute, private router: Router) { }
 
-    criarLista(){
-      if(this.nomeLista != null && this.isPrivate != null && this.idUsuario){
-        this.listaService.createLista(this.idUsuario, this.nomeLista, this.isPrivate, 'sessionid').subscribe(rst => {
-          this.resultadoCriarLista = rst;
-          this.resultadoCriarListaChange.emit(this.resultadoCriarLista);
-        })
-      }
-    }
+  ngOnInit(): void {
+    this.getID();
+  }
 
-    isPrivateChange(target: any){
-      if(target != null){
-        this.isPrivate = target.value
-      }
+  getID() {
+    this.idUsuario = Number(this.route.snapshot.paramMap.get('idSession'));
+  }
+
+  criarLista() {
+    if (this.nomeLista != null && this.isPrivate != null && this.idUsuario) {
+      this.listaService.createLista(this.idUsuario, this.nomeLista, this.isPrivate, 'sessionid').subscribe(rst => {
+        this.resultadoCriarLista = rst;
+        this.resultadoCriarListaChange.emit(this.resultadoCriarLista);
+      })
     }
+  }
+
+  isPrivateChange(target: any) {
+    if (target != null) {
+      this.isPrivate = target.value
+    }
+  }
+
+  cancelar() {
+    this.mostarModal.emit(false);
+  }
 }
