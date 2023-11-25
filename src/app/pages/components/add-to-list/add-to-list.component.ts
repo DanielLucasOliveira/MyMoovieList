a3264b9d283d8ea5d856d4bf59718a194aa560c6
Daniel Lucas <daniel.lucas1507@gmail.com>
2023-11-25 14:26:09 -0300

    ajustando modais

--- src/app/pages/components/add-to-list/add-to-list.component.ts
+++ src/app/pages/components/add-to-list/add-to-list.component.ts
@@ -20,57 +20,59 @@ export class AddToListComponent implements OnInit {
   @Input() cardItem!: CardShow | undefined;
   @Input() criandoItem!: ItemLista;
   @Input() listaSelecionada!: number;
+  @Input() mostrar: boolean = false;
+  @Output() mostrarChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  getID(){
-    if(this.idUsuario == null)
+  getID() {
+    if (this.idUsuario == null)
       this.idUsuario = Number(this.route.snapshot.paramMap.get('idSession'));
   }
 
-  constructor(private listaService: ListaService, private route : ActivatedRoute, private router: Router){}
+  constructor(private listaService: ListaService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.getID()
-    if(this.listaSelecionada != null){
+    if (this.listaSelecionada != null) {
       this.listaService.visualizarLista(this.listaSelecionada).subscribe(rst => {
         this.listasUsuario = Array.of(rst);
       })
-    } else if(this.idUsuario != null){
+    } else if (this.idUsuario != null) {
       this.listaService.visualizarListasUsuario(this.idUsuario).subscribe(rst => {
         this.listasUsuario = rst;
         this.listaSelecionada = this.listasUsuario[0].id;
       });
     }
-    this.criandoItem = {id: null, nome: '', comentario: '', avaliacao: 1, status: 'assistindo', idTmdb: '', urlImagem: ''};
+    this.criandoItem = { id: null, nome: '', comentario: '', avaliacao: 1, status: 'assistindo', idTmdb: '', urlImagem: '' };
   }
 
-  reset(){
+  reset() {
     this.listaService.visualizarListasUsuario(this.idUsuario).subscribe(rst => {
       this.listasUsuario = rst;
       this.listaSelecionada = this.listasUsuario[0].id;
     });
   }
 
-  adicionarItem(){
-    if(this.cardItem != null){
+  adicionarItem() {
+    if (this.cardItem != null) {
       this.criandoItem.idTmdb = this.cardItem.id + '';
       this.criandoItem.nome = this.cardItem.nome;
       this.criandoItem.urlImagem = this.cardItem.urlImagem;
-    } 
-    if(this.criandoItem.id != null){
+    }
+    if (this.criandoItem.id != null) {
       let idLista = this.existeNaLista(this.criandoItem.id);
-      if(idLista != -1){
-        try{
-          this.listaService.editarItem(this.criandoItem.id, this.criandoItem, 'sessionId').subscribe(rs => { 
+      if (idLista != -1) {
+        try {
+          this.listaService.editarItem(this.criandoItem.id, this.criandoItem, 'sessionId').subscribe(rs => {
             this.criandoItem = rs;
             this.resultadoBotaoAdicionar = `${this.cardItem?.nome} editado. item atualizado`;
-            this.resultadoBotaoAdicionarChange.emit(this.resultadoBotaoAdicionar); 
+            this.resultadoBotaoAdicionarChange.emit(this.resultadoBotaoAdicionar);
           });
-        }catch(error){
+        } catch (error) {
           this.resultadoBotaoAdicionar = 'erro';
           this.resultadoBotaoAdicionarChange.emit(this.resultadoBotaoAdicionar);
         }
       }
-    }  else {
+    } else {
       try {
         this.listaService.adicionarItem(this.listaSelecionada, Array.of(this.criandoItem), 'sessionid').subscribe((resultado: string) => {
           this.resultadoBotaoAdicionar = `${this.cardItem?.nome} adicionado. lista atualizada`;
@@ -83,33 +85,38 @@ export class AddToListComponent implements OnInit {
     }
   }
 
-  listaSelecionadaChange(target: any){
-    if(target != null){
+  listaSelecionadaChange(target: any) {
+    if (target != null) {
       this.listaSelecionada = target.value
     }
   }
 
-  statusChange(target: any){
-    if(target != null){
+  statusChange(target: any) {
+    if (target != null) {
       this.criandoItem.status = target.value
     }
   }
 
-  listaVazia(){
+  listaVazia() {
     return this.listasUsuario == null || this.listasUsuario.length === 0;
   }
 
-  existeNaLista(itemId: number): number{
-    
+  existeNaLista(itemId: number): number {
+
     for (const lista of this.listasUsuario) {
       for (const item of lista.itens) {
-        if(item.id == itemId){
+        if (item.id == itemId) {
           return lista.id;
         }
-     }
+      }
     }
 
     return -1;
+  }
+
+  cancelar(){
+    this.mostrar = false;
+    this.mostrarChange.emit(this.mostrar);
   }
 
 }
